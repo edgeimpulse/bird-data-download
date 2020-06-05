c58ebc88b5fb2c8eb171e42a6dcfe290fa397b2f
Daniel Situnayake <dan@edgeimpulse.com>
2020-06-05 16:25:55 -0700

    Working combination of birds

--- upload_all.sh
+++ upload_all.sh
@@ -2,9 +2,7 @@ set -e
 edge-impulse-uploader --label noise --category training data/splits/**/training/noise/*.wav
 edge-impulse-uploader --label titmouse --category training data/splits/**/training/titmouse/*.wav
 edge-impulse-uploader --label mourningdove --category training data/splits/**/training/mourningdove/*.wav
-edge-impulse-uploader --label housefinch --category training data/splits/**/training/housefinch/*.wav
 
 edge-impulse-uploader --label noise --category testing data/splits/**/testing/noise/*.wav
 edge-impulse-uploader --label titmouse --category testing data/splits/**/testing/titmouse/*.wav
 edge-impulse-uploader --label mourningdove --category testing data/splits/**/testing/mourningdove/*.wav
-edge-impulse-uploader --label housefinch --category testing data/splits/**/testing/housefinch/*.wav
